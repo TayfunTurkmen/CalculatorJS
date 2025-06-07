76a99bfdc9c06b3fd424ce32b40dcff0a6e247f7
Tayfun Türkmen <me@tayfun.dev>
2025-06-07 21:39:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ display.value = "";
 
 function calculate(){
     try{
-display.value = eval(display.value);
+ display.value = eval(display.value);
 }
 catch (error){
-
+display.value = "Error";
 }
 }
